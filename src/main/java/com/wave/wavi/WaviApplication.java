d1842fb6a7b18eae4283ec214496c974132886f2
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-07-02 04:29:06 +0900

    환경 설정

---
+++ src/main/java/com/wave/wavi/WaviApplication.java
@@ -0,0 +1,13 @@
+package com.wave.wavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wave/wavi/WaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave.wavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
