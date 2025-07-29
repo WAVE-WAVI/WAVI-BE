f4db86092bce4b3c7cc1eb7904ab64b98c80e506
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-07-30 01:28:52 +0900

    feat: BaseTimeEntity 생성

---
+++ src/main/java/com/wave/wavi/model/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.wave.wavi.model;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @Setter(AccessLevel.PROTECTED)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
