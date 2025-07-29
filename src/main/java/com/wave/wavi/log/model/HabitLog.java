2260a2611f99d93b491814a2eb31a744a035f0b2
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-07-30 03:40:08 +0900

    feat: Habit Log Entity 생성

---
+++ src/main/java/com/wave/wavi/log/model/HabitLog.java
@@ -0,0 +1,64 @@
+package com.wave.wavi.log.model;
+
+import com.wave.wavi.habit.model.Habit;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HabitLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "habitId")
+    private Habit habit;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    @Column
+    private Time startTime;
+
+    @Column
+    private Time endTime;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setHabit(Habit habit) {
+        this.habit = habit;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+}
