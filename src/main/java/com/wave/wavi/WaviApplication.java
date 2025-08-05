44667b30edcd3fcd9fe84ae2463144ea8279e66f
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-06 01:38:26 +0900

    feat: EnableJpaAuditing 설정

--- src/main/java/com/wave/wavi/WaviApplication.java
+++ src/main/java/com/wave/wavi/WaviApplication.java
@@ -2,8 +2,10 @@ package com.wave.wavi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WaviApplication {
 
     public static void main(String[] args) {
