02716dfeea670a37c25ed793b18e8f11daa9c2dd
DESKTOP-2CRI9KO\dodob <k01035675244@gmail.com>
2025-08-14 16:49:34 +0900

    fix: 어노테이션 수정

--- src/main/java/com/wave/wavi/user/dto/UserSignupRequestDto.java
+++ src/main/java/com/wave/wavi/user/dto/UserSignupRequestDto.java
@@ -1,7 +1,11 @@
 package com.wave.wavi.user.dto;
 
 import com.wave.wavi.user.model.LoginType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class UserSignupRequestDto {
     private String email;
     private String password;
