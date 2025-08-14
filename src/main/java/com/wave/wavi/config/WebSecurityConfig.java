4f16abbc4b77c1ffcc2667c1e31ef2336515a2bc
DESKTOP-2CRI9KO\dodob <k01035675244@gmail.com>
2025-08-14 16:49:20 +0900

    fix: passwordEncoder 수정

--- src/main/java/com/wave/wavi/config/WebSecurityConfig.java
+++ src/main/java/com/wave/wavi/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
