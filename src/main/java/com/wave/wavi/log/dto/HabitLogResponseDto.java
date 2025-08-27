8d912452690896143e2d27495d63a102c9db905a
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-28 01:23:10 +0900

    feat: 습관 기록 관련 DTO 수정

--- src/main/java/com/wave/wavi/log/dto/HabitLogResponseDto.java
+++ src/main/java/com/wave/wavi/log/dto/HabitLogResponseDto.java
@@ -4,7 +4,6 @@ import com.wave.wavi.log.model.FailureReason;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.sql.Time;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,7 +14,5 @@ public class HabitLogResponseDto {
     private Long habitId;
     private LocalDate date;
     private boolean completed;
-    private Time startTime;
-    private Time endTime;
     private List<FailureReason> failureReasons;
 }
