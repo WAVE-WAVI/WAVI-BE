0429da0aeb270f53912acd1140674510ab6d5bd8
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-30 00:08:19 +0900

    feat: 습관 등록/조회 유저 연결

--- src/main/java/com/wave/wavi/habit/controller/HabitController.java
+++ src/main/java/com/wave/wavi/habit/controller/HabitController.java
@@ -1,11 +1,11 @@
 package com.wave.wavi.habit.controller;
 
 import com.wave.wavi.common.ResponseDto;
+import com.wave.wavi.config.jwt.JwtUtil;
 import com.wave.wavi.habit.dto.HabitRequestDto;
 import com.wave.wavi.habit.dto.HabitResponseDto;
 import com.wave.wavi.habit.service.HabitService;
-import com.wave.wavi.user.model.LoginType;
-import com.wave.wavi.user.model.User;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,23 +18,14 @@ import java.util.List;
 public class HabitController {
 
     private final HabitService habitService;
+    private final JwtUtil jwtUtil;
 
     // 습관 등록
     @PostMapping("")
-    public ResponseDto<Object> saveHabit(@RequestBody HabitRequestDto requestDto/*, @AuthenticationPrincipal PrincipalDetail principal*/) {
-        // 더미 유저 정보
-        User user = User.builder()
-                .id(1L)
-                .email("test@gmail.com")
-                .loginType(LoginType.NORMAL)
-                .nickname("test")
-                .password("1234")
-                .profileImage(Long.valueOf(2))
-                .build();
-
-        Long habitId = habitService.saveHabit(requestDto, user /*principal.getUser()*/);
+    public ResponseDto<Object> saveHabit(@RequestBody HabitRequestDto requestDto, HttpServletRequest request) {
+        String email = jwtUtil.getUserInfoFromToken(jwtUtil.getTokenFromHeader(request)).get("sub").toString();
+        Long habitId = habitService.saveHabit(requestDto, email);
         habitService.saveDaysOfWeek(requestDto, habitId);
-
         return ResponseDto.builder()
                 .status(HttpStatus.OK.value())
                 .message("습관 등록 성공")
@@ -43,7 +34,7 @@ public class HabitController {
 
     // 습관 수정
     @PatchMapping("/{habitId}")
-    public ResponseDto<Object> updateHabit(@RequestBody HabitRequestDto requestDto, @PathVariable Long habitId/*, @AuthenticationPrincipal PrincipalDetail principal*/) {
+    public ResponseDto<Object> updateHabit(@RequestBody HabitRequestDto requestDto, @PathVariable Long habitId) {
         habitService.updateHabit(requestDto, habitId);
         return ResponseDto.builder()
                 .status(HttpStatus.OK.value())
@@ -53,7 +44,7 @@ public class HabitController {
 
     // 습관 삭제
     @DeleteMapping("/{habitId}")
-    public ResponseDto<Object> deleteHabit(@PathVariable Long habitId/*, @AuthenticationPrincipal PrincipalDetail principal*/) {
+    public ResponseDto<Object> deleteHabit(@PathVariable Long habitId) {
         habitService.deleteHabit(habitId);
         return ResponseDto.builder()
                 .status(HttpStatus.OK.value())
@@ -74,19 +65,9 @@ public class HabitController {
 
     // 내 모든 습관 조회
     @GetMapping("")
-    public ResponseDto<Object> getAllHabits(/*@AuthenticationPrincipal PrincipalDetail principal*/) {
-        // 더미 유저 정보
-        User user = User.builder()
-                .id(1L)
-                .email("test@gmail.com")
-                .loginType(LoginType.NORMAL)
-                .nickname("test")
-                .password("1234")
-                .profileImage(Long.valueOf(2))
-                .build();
-
-        List<HabitResponseDto> habits = habitService.getAllHabits(user);
-
+    public ResponseDto<Object> getAllHabits(HttpServletRequest request) {
+        String email = jwtUtil.getUserInfoFromToken(jwtUtil.getTokenFromHeader(request)).get("sub").toString();
+        List<HabitResponseDto> habits = habitService.getAllHabits(email);
         return ResponseDto.builder()
                 .status(HttpStatus.OK.value())
                 .message("모든 습관 조회 성공")
@@ -96,19 +77,9 @@ public class HabitController {
 
     // 오늘의 습관 조회
     @GetMapping("/today")
-    public ResponseDto<Object> getTodayHabits(/*@AuthenticationPrincipal PrincipalDetail principal*/) {
-        // 더미 유저 정보
-        User user = User.builder()
-                .id(1L)
-                .email("test@gmail.com")
-                .loginType(LoginType.NORMAL)
-                .nickname("test")
-                .password("1234")
-                .profileImage(Long.valueOf(2))
-                .build();
-
-        List<HabitResponseDto> habits = habitService.getTodayHabits(user);
-
+    public ResponseDto<Object> getTodayHabits(HttpServletRequest request) {
+        String email = jwtUtil.getUserInfoFromToken(jwtUtil.getTokenFromHeader(request)).get("sub").toString();
+        List<HabitResponseDto> habits = habitService.getTodayHabits(email);
         return ResponseDto.builder()
                 .status(HttpStatus.OK.value())
                 .message("오늘의 습관 조회 성공")
