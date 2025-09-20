2648b9b39ea413a7a8b90eebd49a388ea2a4ead8
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-09-21 01:45:50 +0900

    docs: HabitReportController 스웨거 문서 작성

--- src/main/java/com/wave/wavi/report/controller/HabitReportController.java
+++ src/main/java/com/wave/wavi/report/controller/HabitReportController.java
@@ -6,6 +6,8 @@ import com.wave.wavi.report.dto.ReportRequestDto;
 import com.wave.wavi.report.model.HabitReport;
 import com.wave.wavi.report.model.ReportType;
 import com.wave.wavi.report.service.HabitReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +19,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/report")
+@Tag(name = "습관 리포트 API", description = "리포트 생성, 조회 API")
 public class HabitReportController {
 
     private final HabitReportService habitReportService;
     private final JwtUtil jwtUtil;
 
     @PostMapping("")
+    @Operation(summary = "리포트 생성", description = "특정 기간의 리포트를 생성하고 저장하는 API")
     public ResponseDto<Object> generateReport(@RequestBody ReportRequestDto requestDto, HttpServletRequest request) {
         String email = jwtUtil.getUserInfoFromToken(jwtUtil.getTokenFromHeader(request)).getSubject();
         habitReportService.generateReport(requestDto, email);
@@ -33,6 +37,7 @@ public class HabitReportController {
     }
 
     @GetMapping("")
+    @Operation(summary = "조건별 모든 리포트 조회", description = "타입, 날짜 범위로 리포트를 조회하는 API")
     public ResponseDto<Object> getReports(
             @RequestParam (name = "type", required = false) ReportType type,
             @RequestParam (name = "startDate", required = false) LocalDate startDate,
